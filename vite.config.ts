7fbd7cbabdf250c5c506a93fe953caa945b2e07d
Anastasia Konushok <nastjenka.kon@gmail.com>
2024-07-11 17:06:54 +0300

    fix: vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
   const __dirname = fileURLToPath(new URL('.', import.meta.url));
   return {
     plugins: [react()],
-    base: "/rss_react/",
+    base: "/rs_react/",
     resolve: {
       alias: [
         { find: "@src", replacement: resolve(__dirname, "./src") },
